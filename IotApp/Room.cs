cb63c0525505b31480964c39832fd665fd48886b
Basile David <basiledavid.2000@gmail.com>
2023-05-26 09:39:36 +0200

    room seat class init

---
+++ IotApp/Room.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotApp
+{
+    internal class Room
+    {
+        /*Attribute*/
+        private int deafPeoplePer = 6;
+        private int roomId;
+        public int RoomCapacity { get; }
+        public List<Seat> SeatList = new List<Seat>();
+
+        public Room(int roomNb, int nbSeat)
+        {
+            roomId = roomNb;
+            RoomCapacity = nbSeat; 
+
+            for (int i = 0; i <= roomNb; i++)
+            {
+                //use to determine the number of equiped seats 
+                if (i%deafPeoplePer == 0)
+                {
+                    SeatList.Add(new Seat(i,roomNb,true));
+                }
+                else
+                {
+                    SeatList.Add(new Seat(i,roomNb,false));
+                }
+            }
+        }
+
+        
+
+    }
+}
---
+++ IotApp/Seat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotApp
+{
+    internal class Seat
+    {
+        /*Attribute*/
+        private int roomId;
+        public int SeatId { get; }
+        public bool IsOccupied { get; set; }
+        public bool IsAudioDesc { get;}
+
+
+        public Seat(int seatId, int roomNB, bool audioDesc) 
+        {
+            SeatId = seatId;
+            roomId = roomNB;
+            IsOccupied = false;
+
+        }
+
+
+    }
+}
